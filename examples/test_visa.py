88b40ee26fcf3a30e667263a2012f7cad85a2ec9
rbh <rosshutson@gmail.com>
2022-03-15 17:44:55 -0600

    echo

--- examples/test_visa.py
+++ examples/test_visa.py
@@ -2,8 +2,10 @@ from labd.libs._visa import VisaProxy
 
 #visa = VisaProxy(('192.168.1.49', 42922))
 visa = VisaProxy(('192.168.107.58', 42922))
+message = visa._echo('hello!')
 rm = visa.ResourceManager()
 print(rm.list_resources())
+
 #inst = rm.open_resource("GPIB0::2::INSTR")
 #print(inst.query("*IDN?"))
 #inst.timeout = 1000
